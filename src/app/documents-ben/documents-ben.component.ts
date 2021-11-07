e161693ffb256bb19c67c2b7bf538387712cebb7
CristhianCastillo <cece9707@gmail.com>
2021-11-07 12:06:27 -0500

    Se realizan ajustes de Cargar de Archivos

--- src/app/documents-ben/documents-ben.component.ts
+++ src/app/documents-ben/documents-ben.component.ts
@@ -6,60 +6,61 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-documents-ben',
   templateUrl: './documents-ben.component.html',
-  styleUrls: ['./documents-ben.component.css']
+  styleUrls: ['./documents-ben.component.css'],
 })
 export class DocumentsBenComponent implements OnInit {
+  public adresFile?: File;
+  public sisbenFile?: File;
+  public progress = 0;
+  public message = '';
+  public fileInfos?: Observable<any>;
 
-  selectedFiles?: FileList;
-  currentFile?: File;
-  progress = 0;
-  message = '';
+  constructor(private uploadService: UploadFilesService) {}
 
-  fileInfos?: Observable<any>;
-
-  constructor(private uploadService: UploadFilesService) { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.fileInfos = this.uploadService.getFiles();
   }
 
-  selectFile(event: any): void {
-    this.selectedFiles = event.target.files;
+  public selectAdresFile(event: any): void {
+    const selectedFiles = event.target.files;
+    if (selectedFiles) {
+      this.adresFile = selectedFiles.item(0);
+    }
   }
 
-  upload(): void {
+  public selectSisbenFile(event: any): void {
+    const selectedFiles = event.target.files;
+    if (selectedFiles) {
+      this.sisbenFile = selectedFiles.item(0);
+    }
+  }
+
+  public upload(): void {
     this.progress = 0;
-  
-    if (this.selectedFiles) {
-      const file: File | null = this.selectedFiles.item(0);
-  
-      if (file) {
-        this.currentFile = file;
-  
-        this.uploadService.upload(this.currentFile).subscribe(
-          (event: any) => {
-            if (event.type === HttpEventType.UploadProgress) {
-              this.progress = Math.round(100 * event.loaded / event.total);
-            } else if (event instanceof HttpResponse) {
-              this.message = event.body.message;
-              this.fileInfos = this.uploadService.getFiles();
-            }
-          },
-          (err: any) => {
-            console.log(err);
-            this.progress = 0;
-  
-            if (err.error && err.error.message) {
-              this.message = err.error.message;
-            } else {
-              this.message = 'Could not upload the file!';
-            }
-  
-            this.currentFile = undefined;
-          });
-      }
-  
-      this.selectedFiles = undefined;
+    if (this.adresFile && this.sisbenFile) {
+      this.uploadService.upload(this.adresFile, this.sisbenFile).subscribe(
+        (event: any) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progress = Math.round((100 * event.loaded) / event.total);
+          } else if (event instanceof HttpResponse) {
+            this.message = event.body.message;
+            // this.fileInfos = this.uploadService.getFiles();
+            console.log('Response service: ', event.body);
+          }
+        },
+        (err: any) => {
+          console.log(err);
+          this.progress = 0;
+
+          if (err.error && err.error.message) {
+            this.message = err.error.message;
+          } else {
+            this.message = 'Could not upload the file!';
+          }
+        }
+      );
+    } else {
+      this.message = 'Please select files!';
     }
   }
 }
--- src/app/services/upload-files.service.ts
+++ src/app/services/upload-files.service.ts
@@ -3,24 +3,22 @@ import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UploadFilesService {
-
   private baseUrl = 'http://localhost:8082/cao-core-api/v1/files';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  
-  upload(file: adresFile, sisbenFile): Observable<HttpEvent<any>> {
+  upload(adresFile: File, sisbenFile: File): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
 
-    formData.append('file', adresFile);
-    formData.append('file', sisbenFile);
+    formData.append('imageAdres', adresFile);
+    formData.append('imageSisben', sisbenFile);
 
     const req = new HttpRequest('POST', `${this.baseUrl}`, formData, {
       reportProgress: true,
-      responseType: 'json'
+      responseType: 'json',
     });
 
     return this.http.request(req);
